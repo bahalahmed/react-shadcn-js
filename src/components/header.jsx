abb3e42218a4f64369bdf9f63a17768cea6705c8
Bahal Ahmad <khushibaby.org>
2025-02-12 15:29:16 +0530

    used drop down componenets

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,40 @@
 import {Button} from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 import { Link, useNavigate } from 'react-router-dom'
 
 const Header = () => {
   const navigate = useNavigate()
+  const user = true;
   return (
     <nav className=' py-4 flex justify-between items-center'>
         <Link to=''>
         <img src = '/logo.png' className='h-16' alt = 'logo' />
         </Link>
         <div>
+          {!user ? 
           <Button onClick={() => navigate('/auth')}>Login</Button>
+          :(
+            <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          
+          )}
           </div>
     </nav>
   )
