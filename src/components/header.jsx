fe312c9ddc62cdef8c5e6aa3953cf4717ad113d4
Bahal Ahmad <khushibaby.org>
2025-02-12 15:38:47 +0530

    used avatar and lucid react icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
-import {Button} from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,38 +6,46 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
+import { LinkIcon, LogOut } from "lucide-react";
 
 const Header = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const user = true;
   return (
-    <nav className=' py-4 flex justify-between items-center'>
-        <Link to=''>
-        <img src = '/logo.png' className='h-16' alt = 'logo' />
-        </Link>
-        <div>
-          {!user ? 
-          <Button onClick={() => navigate('/auth')}>Login</Button>
-          :(
-            <DropdownMenu>
-            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+    <nav className=" py-4 flex justify-between items-center">
+      <Link to="">
+        <img src="/logo.png" className="h-16" alt="logo" />
+      </Link>
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>BA</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuLabel>Bahal Ahmad</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" /> My Links
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <LogOut className="mr-2 h-4 w-4" /> <span>Logout</span>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          
-          )}
-          </div>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
