b6a63aa065b446eb5aed4b34645c9e314ee080df
Bahal Ahmad <khushibaby.org>
2025-02-12 15:25:34 +0530

    used add components

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import {Button} from '@/components/ui/button'
+import { Link, useNavigate } from 'react-router-dom'
+
 const Header = () => {
+  const navigate = useNavigate()
   return (
-    <nav>
-        <Link>
+    <nav className=' py-4 flex justify-between items-center'>
+        <Link to=''>
         <img src = '/logo.png' className='h-16' alt = 'logo' />
         </Link>
+        <div>
+          <Button onClick={() => navigate('/auth')}>Login</Button>
+          </div>
     </nav>
   )
 }
